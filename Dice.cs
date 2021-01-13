c5959709f5a574136348ac204930340e64177248
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-13 15:32:44 +0200

    add file with example of constructor

---
+++ Dice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectsAndClasses
+{
+    class Dice
+    {
+                       // Constructor is a special method that creates the object of the class
+        public Dice()   // kind a default contructor, gives the initial values of the object
+        {
+            this.Sides = 6;
+        }
+        public Dice(int sides)   // this is defined by us consructor. /creates an instance object 
+        {                        // sets the start state of the object/
+            Sides = sides;
+        }
+
+        public int Sides { get; set; }
+        public string Type { get; set; }
+
+        public int Roll() 
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, this.Sides + 1); // till 6+1 till 6 inclusive, if we say just side we can not have ever 6;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ObjectsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice6 = new Dice(6);
+            Dice diceDnD = new Dice(20);
+            
+            for (int i = 0; i < 15 ; i++)
+            {
+                Console.WriteLine(dice6.Roll());
+
+            }
+        }
+    }
+}
